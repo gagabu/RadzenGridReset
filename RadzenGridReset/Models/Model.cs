e1d1c4a348e077d288bdb38974a3b3c8a807ab0e
gagabu <booluk@gmail.com>
2022-09-23 14:35:05 +0300

    grid resets page

---
+++ RadzenGridReset/Models/Model.cs
@@ -0,0 +1,7 @@
+﻿namespace RadzenGridReset.Models;
+
+public sealed record Model
+{
+	public string Name { get; set; } = string.Empty;
+	public int Number { get; set; }
+}
---
+++ RadzenGridReset/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using RadzenGridReset.Models;
+
+namespace RadzenGridReset.Pages;
+
+public partial class Index : ComponentBase
+{
+	private List<Model> Data { get; set; } = new();
+
+	protected override void OnInitialized()
+	{
+		for (int i = 0; i < 20; i++)
+		{
+			Data.Add(new Model
+			{
+				Name = RandomString(10),
+				Number = Random.Shared.Next()
+			});
+		}
+	}
+
+	private static string RandomString(int length)
+	{
+		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		return new string(Enumerable.Repeat(chars, length)
+			.Select(s => s[Random.Shared.Next(s.Length)]).ToArray());
+	}
+}
